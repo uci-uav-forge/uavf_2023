0b5e8e90724bfd452b2f43890950f3351ad3be81
MichaelKrinsky <34221696+MichaelKrinsky@users.noreply.github.com>
2022-10-24 11:18:01 -0700

    Finished data generator

--- imaging/letter-detection/generateData.py
+++ imaging/letter-detection/generateData.py
@@ -1,54 +1,105 @@
 import pygame
 import os
-ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+import math
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
 
-# Create file directories 
-for x in ALPHABET:
-    try:
-        os.mkdir(f'./data/{x}')
-    except:
-        pass
-pygame.init()
-clock = pygame.time.Clock()
-resolution = 128
-
-
-def main():
-    # Create file directories 
-    for x in ALPHABET:
-        try:
-            os.mkdir(f'./data/{x}')
-        except:
-            pass
-    # choose = int(input("Please enter num font: "))
-    choose = 2
-    with open("fonts.txt", "r") as file:
-        f = file.readlines()
-    font = f[choose].strip()
-    print(f"you have chosen: {font}")
-    generate_data(font)
+# # Create file directories 
+# for x in ALPHABET:
+#     try:
+#         os.mkdir(f'./data/{x}')
+#     except:
+#         pass
+# pygame.init()
+# clock = pygame.time.Clock()
+# resolution = 128
 
 
-def generate_data(font, changeDeg = 15, path=""):
-    pygame.init()
-    resolution = 128
-    changeDeg = 15
-    currentDeg = 0
-    numPhotos = 360 // changeDeg
-    font = pygame.font.SysFont(font, resolution)
-    screen = pygame.display.set_mode([resolution,resolution])
+# def main():
+#     # Create file directories 
+#     for x in ALPHABET:
+#         try:
+#             os.mkdir(f'./data/{x}')
+#         except:
+#             pass
+#     # choose = int(input("Please enter num font: "))
+#     choose = 2
+#     with open("fonts.txt", "r") as file:
+#         f = file.readlines()
+#     font = f[choose].strip()
+#     print(f"you have chosen: {font}")
+#     generate_letters(font)
+
+class DataGenerator():
     
-    for x in ALPHABET: 
-        text = x
-        for number in range(numPhotos):
+    def __init__(self, resolution=128):
+        self.resolution = 128
+        pygame.init()
+        self.screen = pygame.display.set_mode([self.resolution,resolution])
 
-            screen.fill((0,0,0)) # Fill screen with black
-            letter = font.render(text, True, (255,255,255), (0,0,0))
+
+    def generate_letter(self, font, letter, numImages = 15, path="", startNum = 0):
+        '''Generates data for the given font and number of images'''
+        changeDeg = 360 // numImages
+        currentDeg = 0
+        font = pygame.font.SysFont(font, self.resolution)
+        for number in range(numImages):
+            self.screen.fill((0,0,0)) # Fill screen with black
+            letter = font.render(letter, True, (255,255,255), (0,0,0))
             letter = pygame.transform.rotate(letter, currentDeg)
             currentDeg += changeDeg
             textRect = letter.get_rect()
-            textRect.center = (resolution // 2, resolution // 2)
-            screen.blit(letter, textRect)
+            textRect.center = (self.resolution // 2, self.resolution // 2)
+            self.screen.blit(letter, textRect)
             pygame.display.flip()
-            pygame.image.save(screen, f"{path}/data/{text}/{number}.jpg")
-    pygame.quit()
+            pygame.image.save(self.screen, f"{path}/data/{letter}_{startNum + number}.jpg")
+    
+
+    def generate_letters(self, font, distribution, numImages, path="", labelPath=".\dataset\labels.txt", startNum = 0):
+        print(numImages, startNum)
+        with open(labelPath, "a") as file:
+            currentDeg = 0
+            font = pygame.font.SysFont(font, self.resolution)
+            screen = pygame.display.set_mode([self.resolution,self.resolution])
+            
+            for index, x in enumerate(ALPHABET): 
+                text = x
+                numPhotos = math.ceil(distribution[text] * numImages)
+                for _ in range(numPhotos):
+                    
+                    changeDeg = 360 / numPhotos
+                    screen.fill((0,0,0)) # Fill screen with black
+                    letter = font.render(text, True, (255,255,255), (0,0,0))
+                    letter = pygame.transform.rotate(letter, currentDeg)
+                    currentDeg += changeDeg
+                    textRect = letter.get_rect()
+                    textRect.center = (self.resolution // 2, self.resolution // 2)
+                    screen.blit(letter, textRect)
+                    pygame.display.flip()
+                    pygame.image.save(screen, f"{path}\{startNum}.jpg")
+                    file.write(f"{startNum}.jpg" + ", " + str(index) + "\n")
+                    startNum += 1
+
+
+    # def generate_data(self, font, numImages = 15, path=""):
+    #     '''Generates data for the given font and number of images'''
+    #     pygame.init()
+    #     changeDeg = 360 // numImages
+    #     currentDeg = 0
+    #     numPhotos = 360 // changeDeg
+    #     font = pygame.font.SysFont(font, self.resolution)
+    #     screen = pygame.display.set_mode([self.resolution,self.resolution])
+        
+    #     for x in ALPHABET: 
+    #         text = x
+    #         for number in range(numPhotos):
+
+    #             screen.fill((0,0,0)) # Fill screen with black
+    #             letter = font.render(text, True, (255,255,255), (0,0,0))
+    #             letter = pygame.transform.rotate(letter, currentDeg)
+    #             currentDeg += changeDeg
+    #             textRect = letter.get_rect()
+    #             textRect.center = (self.resolution // 2, self.resolution // 2)
+    #             screen.blit(letter, textRect)
+    #             pygame.display.flip()
+    #             pygame.image.save(screen, f"{path}/data/{text}/{number}.jpg")
+    #     pygame.quit()
--- imaging/letter-detection/generateDataset.py
+++ imaging/letter-detection/generateDataset.py
@@ -1,7 +1,66 @@
+import generateData
+import os
+import math
+class DataSet():
+    def __init__(self, size):
+        self.createFilesAndDirectories()
+        self.size = size
+        self.labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+        self.numLabels = 36
+        self.distributions = dict()
+        self.remaining = 1
+        self.generater = generateData.DataGenerator(128)
+
+
+    def createFilesAndDirectories(self):
+        '''Creates file hierarchy needed to run'''
+        try:
+            os.mkdir('.\dataset')
+        except Exception: # directories already exist
+            pass
+        try:
+            os.mkdir('.\dataset\data')
+        except Exception: # directories already exist
+            pass
+        with open(".\dataset\labels.txt", "w") as f:
+            f.write("file, label\n")
+
+
+    def setRemainingDistributions(self):
+        '''Sets the remaining distributions given that the customs are filled already'''
+        photoDistribution = self.remaining / (self.numLabels - len(self.distributions)) 
+        for label in self.labels:
+            if(label not in self.distributions):
+                self.distributions[label] = photoDistribution
+                self.remaining -= photoDistribution
+    
+
+    def setDistribution(self, key, dist):
+        '''Sets a distribution for the given key'''
+        if(key in self.distributions):
+            self.remaining += self.distributions[key]
+        if(0 < dist <= self.remaining):
+            self.distributions[key] = dist
+            self.remaining -= dist
+        else:
+            raise Exception("Not enough space for the new distribution: ",self.remaining, dist)
+
+
+    def generate(self, path=""):
+        with open("myfonts.txt", "r") as file:
+            fonts : list = file.readlines()
+        numPhotos = math.ceil(self.size /len(fonts))
+        print(numPhotos)
+        for x in range(len(fonts)):
+
+            self.generater.generate_letters(fonts[x], self.distributions, numPhotos, path =".\dataset\data", labelPath=".\dataset\labels.txt", startNum = (x * numPhotos))
+        
 
 def main():
-    with open("myfonts.txt" "r") as file:
-        fonts : list = file.readlines()
+    d = DataSet(2000)
+    d.setRemainingDistributions()
+    d.generate()
+
 
     
 
