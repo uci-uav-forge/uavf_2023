dbcc6fd322e45cae963d6c3b80e4160a4ffcef65
Your Name <kvanuy@uci.edu>
2022-10-24 16:17:17 -0700

    imaging branch files targetinfo and dronestatus

---
+++ imaging/DroneStatus.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from collections import namedtuple
+
+@dataclass
+class DroneStatus():
+    GPSCoord: tuple(float,float))
+    Altitude: float
+    Pitch: float
+    Yaw: float
+    Roll: float
+
+    def updatecoord(self, newcoords):
+        formatgps = namedtuple('Location', ['lat' , 'long' ])
+        self.GPSCoord = formatgps(newcoords[0],newcoords[0])
+    
+
+
+def testrun():
+
+    droneexamplestatus = DroneStatus((0,0), 10, 9,  8,  7)
+    print(droneexamplestatus)
+
+
+testrun()
---
+++ imaging/TargetInfo.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass, InitVar
+from collections import namedtuple
+from typing import Optional
+
+@dataclass
+class TargetInfo:
+    Shape: str
+    Colors: tuple([str, str])
+    Letter: str
+    CenterCoord: tuple([float,float])
+    CalcGPSCoord: InitVar[Optional[tuple]] = None
+
+
+    def updateGPS(self,missingcoord):
+            self.CalcGPSCoord = missingcoord
+
+
+
+
+targetexampleinfo = TargetInfo("triangle",("blue","black"),"A", (4,6))
+print(targetexampleinfo)
+targetexampleinfo.updateGPS((500,200))
+print(targetexampleinfo)
+print(targetexampleinfo.CalcGPSCoord)
+        
+    
+
+
+
+
+
