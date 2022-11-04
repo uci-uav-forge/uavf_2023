a6dfafc23b243f315ef97a09dc1164e956864d10
Shannon Liou <sch9050@gmail.com>
2022-11-04 15:23:35 -0700

    Added slModel.py

---
+++ imaging/letter-detection/slModel.py
@@ -0,0 +1,68 @@
+from letterDetectionModel import *
+from collections import defaultdict
+import keras
+
+model = keras.Sequential([
+    keras.layers.Conv2D(16, (3, 3), input_shape = (128,128,1), activation = 'relu'),
+    keras.layers.MaxPooling2D(pool_size = (2,2)),
+    keras.layers.Conv2D(32, (3, 3), activation = 'relu'),
+    keras.layers.MaxPooling2D(pool_size = (2,2)),
+    #keras.layers.Dense(64, activation = 'relu'),
+    #keras.layers.Conv2D(64, (3, 3), activation = 'relu'),
+    #keras.layers.MaxPooling2D(pool_size = (2,2)),
+    keras.layers.Flatten(),
+    #keras.layers.Dense(256, activation = 'relu'),
+    #keras.layers.Dense(256, activation = 'relu'),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(128, activation = 'relu'),
+    keras.layers.Dense(35)
+    
+])
+
+model.compile(optimizer=tf.optimizers.Adam(),
+                loss=[keras.losses.SparseCategoricalCrossentropy(from_logits=True),], 
+                metrics="accuracy")
+
+model.fit(ds_train, epochs=3, shuffle=True)
+print("Evaluate: ")
+result = model.evaluate(ds_test)
+result = model.predict(ds_test)
+# result is a 2D matrix: each row represents each image, and each column represents the "likelihood" for each key
+# find the max column value for each row
+
+predict = []
+for row in range(len(result)):
+    max = 0
+    max_col = 0
+    i = 0
+    for col in range(len(result[0])):
+        if result[row][col] > max:
+            max = result[row][col]
+            max_col = col
+    predict.append(max_col)
+
+#wrong_predictions = []
+#for i in range(len(predict)):
+#    if predict[i] != test_labels[i]:
+#        wrong_predictions.append((chr(test_labels[i]), chr(predict[i])))
+#print(wrong_predictions)
+d = {}
+for i in range(len(predict)):
+    file_path = test_file_paths[i]
+    prediction = predict[i]
+    actual_label = test_labels[i]
+    d.update({file_path: (prediction, actual_label)})
+
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+def evaluateChars(predictions:dict[tuple[int,int]]):
+    numCorrect = defaultdict(int)
+    totals = defaultdict(int)
+    for predicted, correct in predictions.values():
+        numCorrect[correct]
+        totals[correct] += 1
+        if(predicted == correct):
+            numCorrect[correct] += 1
+    for letter, (n, t) in enumerate(zip(numCorrect.values(),totals.values())):
+        print(alpha[letter], ":", n,"out of", t)
+    
+evaluateChars(d)
