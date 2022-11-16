1017cb9ee4915621c311f201bf3f3ff4a6ea883f
MichaelKrinsky <34221696+MichaelKrinsky@users.noreply.github.com>
2022-11-16 10:17:29 -0800

    lowered epochs

--- imaging/letter-detection/mkModel.py
+++ imaging/letter-detection/mkModel.py
@@ -38,6 +38,6 @@ model.compile(optimizer=tf.optimizers.Adam(),
                 
 # log_dir = "logs/fit/slModel0_18"
 # tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-model.fit(ds_train, epochs=20)
+model.fit(ds_train, epochs=8)
 
 model.save("./trained_model.h5")
