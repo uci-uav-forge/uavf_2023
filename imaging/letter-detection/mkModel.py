447e46d14151aff8bdca1d2150e2da4971c20f40
MichaelKrinsky <34221696+MichaelKrinsky@users.noreply.github.com>
2022-11-17 17:06:41 -0800

    added command to save as onnx

--- imaging/letter-detection/mkModel.py
+++ imaging/letter-detection/mkModel.py
@@ -39,6 +39,10 @@ model.compile(optimizer=tf.optimizers.Adam(),
                 
 # log_dir = "logs/fit/slModel0_18"
 # tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-model.fit(ds_train, epochs=20)
-model.save("./trained_model")
+model.fit(ds_train, epochs=1)
 
+model.save("./trained_model")
+# onnx_model_name = 'fish-resnet50.onnx'
+
+# onnx_model = keras2onnx.convert_keras(model, model.name)
+# onnx.save_model(onnx_model, onnx_model_name)
