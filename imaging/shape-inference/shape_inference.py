fd07bad49184476c32fbebe9bfceeae5a8ed8073
Eric Pedley <ericpedley@gmail.com>
2022-11-28 21:28:39 -0800

    add start of NMS code

--- imaging/shape-inference/shape_inference.py
+++ imaging/shape-inference/shape_inference.py
@@ -35,6 +35,36 @@ def make_prediction(img: np.ndarray) -> List[np.ndarray]:
     outputs = ort_sess.run(None, {"images": model_input.astype(np.uint8)})
     return outputs
 
+def supress_nonmax_bboxes(bbox_list: np.ndarray, iou_threshold):
+    '''
+    bbox_list should be like this:
+    [
+        [x1,y1,x2,y2]
+    ]
+    and sorted by confidence, with the highest confidence boxes first in the list
+    x1 and y1 are the top left, and x2 and y2 are the bottom right
+    
+    iou threshold should be a float between 0 and 1.
+    if the intersection-over-union (iou) of two boxes is > threshold, it'll discard the one with lower confidence.
+    '''
+    correct_bboxes = []
+    print(bbox_list)
+    for x1,y1,x2,y2 in bbox_list:
+        is_duplicate = False
+        for x2,y2,w2,h2 in correct_bboxes:
+            if (x2<=x1<=x2+w2 and y2<=y1<=y2+h2) or (x2<=x1+w1<=x2+w2 and y2<=y1+h1<=y2+h2):
+                # gets the coordinates of the intersection
+                p1, p2 = sorted([(x1,y1),(x1+w1,y1+h1),(x2,y2),(x2+w2,y2+h2)])[1:3]
+                intersection_area = abs((p1[0]-p2[0])*(p1[1]-p2[1]))
+                a1 = w1*h1
+                a2 = w2*h2
+                iou = intersection_area/(a1+a2-intersection_area)
+                if iou >= iou_threshold:
+                    is_duplicate=True
+        if not is_duplicate:
+            correct_bboxes.append((x1,y1,w1,h1))
+    return correct_bboxes
+
 if __name__=="__main__":
     img = Image.open("example-shape.jpg")
     outputs: list[np.ndarray] = make_prediction(img)
