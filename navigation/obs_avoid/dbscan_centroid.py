a8e5986b807df840d0d0e7f9996582af83493b6b
Derek Fan <herpderkfanie@gmail.com>
2023-01-20 03:57:26 -0800

    full pcd processing pipeline

--- navigation/obs_avoid/dbscan_centroid.py
+++ navigation/obs_avoid/dbscan_centroid.py
@@ -35,8 +35,8 @@ def main() -> None:
     num_of_clusters = labels.max() + 1
     points = np.asarray(cl.points)
     st = time.time()
-    centroids, cluster_arr = find_centroids(num_of_clusters, points, labels)
-    print(cluster_arr )
+    centroids, cluster_tup = find_centroids(num_of_clusters, points, labels)
+    print(cluster_tup )
     print('centroid calculation runtime: ' + str(time.time()-st))
     print(f'{num_of_clusters} found. centroids of clusters in [x,y,z] form:')
     print(centroids)
@@ -50,17 +50,17 @@ def main() -> None:
     visualize_pcd_and_centroids(pcd=cl, centroids=centroids)
     
 
-def find_centroids(num_of_clusters: int, points: np.ndarray, labels: np.ndarray) -> tuple:
+def find_centroids(num_cluster: int, points: np.ndarray, labels: np.ndarray) -> tuple:
     '''Will return an array containing all centroids [[x1,y1,z1], [x2,y2,z2], ...]
        as well as a tuple containing n-by-3 arrays of points belonging to nth cluster.
        The cluster arrays are stored in tuples to allow for storage in a non-rectangular shape
        i.e. cluster arrays with differing numbers of points'''
     
     # initialize centroid array and cluster tuple
-    centroids = np.zeros((num_of_clusters, 3), dtype=float)       
+    centroids = np.zeros((num_clusters, 3), dtype=float)       
     cluster_tup = ()
 
-    for i in range(num_of_clusters):
+    for i in range(num_clusters):
         # get indices of points belonging to ith cluster in array of all points
         indx_arr = np.where(labels == i)[0]
         cluster = np.zeros((indx_arr.size, 3), dtype=float)
@@ -68,22 +68,10 @@ def find_centroids(num_of_clusters: int, points: np.ndarray, labels: np.ndarray)
         for j in range(indx_arr.size):
             cluster[j] = points[indx_arr[j]]  
         # concatenate cluster array to tuple
-        cluster_tup += (cluster,)
         centroids[i] = np.average(cluster, axis=0)
+        cluster_tup += (cluster,)
     
     return centroids, cluster_tup
-
-
-def bound_box(positions: np.ndarray) -> np.ndarray:
-    p0 = np.amin(positions, 0)
-    p1 = np.amax(positions, 0)
-    p2 = np.array([p1[0], p0[1], p0[2]])
-    p3 = np.array([p0[0], p1[1], p0[2]])
-    p4 = np.array([p1[0], p1[1], p0[2]])
-    p5 = np.array([p0[0], p0[1], p1[2]])
-    p6 = np.array([p1[0], p0[1], p1[2]])
-    p7 = np.array([p0[0], p1[1], p1[2]])
-    return np.array([p0, p1, p2, p3, p4, p5, p6, p7])
 
 
 def visualize_pcd_and_centroids(pcd: o3d.geometry.PointCloud, centroids: list) -> list:
--- navigation/obs_avoid/open3d_test.py
+++ navigation/obs_avoid/open3d_test.py
@@ -1,6 +1,6 @@
 # sources:
-# for downsampling: http://www.open3d.org/docs/latest/tutorial/Basic/pointcloud.html#Visualize-point-cloud
-# for outlier removal: http://www.open3d.org/docs/latest/tutorial/Advanced/pointcloud_outlier_removal.html
+# downsampling: http://www.open3d.org/docs/latest/tutorial/Basic/pointcloud.html#Visualize-point-cloud
+# outlier removal: http://www.open3d.org/docs/latest/tutorial/Advanced/pointcloud_outlier_removal.html
 
 
 import open3d as o3d
@@ -9,114 +9,88 @@ import time
 import matplotlib.pyplot as plt
 
 
-def find_centroids(num_of_clusters: int, points: np.ndarray, labels: np.ndarray) -> np.ndarray:
-    '''will return an array containing all centroids [[x1,y1,z1], [x2,y2,z2], ...]'''
-
-    cluster_arr = np.empty((num_of_clusters, len(points), 3), dtype=float)
-    sums = np.empty((num_of_clusters, 3), dtype=float)
-    counter = np.empty(num_of_clusters, dtype=float)
-
-    for i in range(len(points)):
-        for j in range(num_of_clusters):
-            if labels[i] == j:
-                counter[j] += 1 # update counter
-                cluster_arr[j][counter[j]] = points[i]
-                sums[j] += points[i]
+def segment_clusters(num_cluster: int, points: np.ndarray, labels: np.ndarray) -> tuple:
+    '''Will return an array containing all centroids [[x1,y1,z1], [x2,y2,z2], ..., [xN,yN,zN]]
+       as well as a tuple containing N clusters.'''
     
-    # now calculate averages:
-    print(cluster_arr)
-    return sums / counter[:, None]
+    # initialize centroid array and cluster tuple
+    centr_arr = np.zeros((num_cluster, 3), dtype=float)       
+    pcd_tup = ()
 
-
-# visualization
-dataset = o3d.data.PLYPointCloud()
-pcd = o3d.io.read_point_cloud(dataset.path)
-print(pcd)
-o3d.visualization.draw_geometries([pcd],
-                                  zoom=0.3412,
-                                  front=[0.4257, -0.2125, -0.8795],
-                                  lookat=[2.6172, 2.0475, 1.532],
-                                  up=[-0.0694, -0.9768, 0.2024])
-
-
-# downsample
-print("Downsample the point cloud with a voxel of 0.05")
-st = time.time()
-downpcd = pcd.voxel_down_sample(voxel_size=0.1)
-downsample_time = time.time() - st
-print('downsampling runtime: ' + str(downsample_time))
-o3d.visualization.draw_geometries([downpcd],
-                                  zoom=0.3412,
-                                  front=[0.4257, -0.2125, -0.8795],
-                                  lookat=[2.6172, 2.0475, 1.532],
-                                  up=[-0.0694, -0.9768, 0.2024])
-
-'''
-# color point cloud
-print("Paint chair")
-pcd.paint_uniform_color([1, 0, 0])
-o3d.visualization.draw_geometries([downpcd],
-                                  zoom=0.3412,
-                                  front=[0.4257, -0.2125, -0.8795],
-                                  lookat=[2.6172, 2.0475, 1.532],
-                                  up=[-0.0694, -0.9768, 0.2024])
-'''
-
-
-# radius outlier removal
-print("Radius oulier removal")
-st = time.time()
-cl, ind = downpcd.remove_radius_outlier(nb_points=16, radius=0.2)
-outlier_time = time.time() - st
-print('outlier removal time' + str(outlier_time))
-o3d.visualization.draw_geometries([cl],
-                                    zoom=0.3412,
-                                    front=[0.4257, -0.2125, -0.8795],
-                                    lookat=[2.6172, 2.0475, 1.532],
-                                    up=[-0.0694, -0.9768, 0.2024])
+    for i in range(num_cluster):
+        # get indices of points belonging to ith cluster in array of all points
+        indx_arr = np.where(labels == i)[0]
+        cluster = np.zeros((indx_arr.size, 3), dtype=float)
+        # insert the points at each of those indices into the cluster array
+        for j in range(indx_arr.size):
+            cluster[j] = points[indx_arr[j]]
+        # convert cluster array to pcd and concatenate to tuple
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(cluster)
+        pcd_tup += (pcd,)
+        centr_arr[i] = np.average(cluster, axis=0)
     
-
-# DBSCAN clustering
-st = time.time()
-with o3d.utility.VerbosityContextManager(
-        o3d.utility.VerbosityLevel.Debug) as cm:
-    labels = np.array(
-        cl.cluster_dbscan(eps=0.2, min_points=10, print_progress=False))
-cluster_time = time.time() - st
-print("cluster computing time: " + str(cluster_time))
+    return centr_arr, pcd_tup
 
 
-print(labels)
-max_label = labels.max()
-print(f"point cloud has {max_label + 1} clusters")
-colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
-colors[labels < 0] = 0
-cl.colors = o3d.utility.Vector3dVector(colors[:, :3])
-o3d.visualization.draw_geometries([cl],
-                                  zoom=0.455,
-                                  front=[-0.4999, -0.1659, -0.8499],
-                                  lookat=[2.1813, 2.0619, 2.0999],
-                                  up=[0.1204, -0.9852, 0.1215])
+if __name__ == '__main__':
+    # visualization
+    dataset = o3d.data.PLYPointCloud()
+    pcd = o3d.io.read_point_cloud(dataset.path)
+    print(pcd)
+    print()
+    o3d.visualization.draw_geometries([pcd])
 
+    # downsample
+    print("Downsample the point cloud with a voxel of 0.05")
+    st = time.time()
+    downpcd = pcd.voxel_down_sample(voxel_size=0.1)
+    downsample_time = time.time() - st
+    print('downsampling runtime: ' + str(downsample_time))
+    print()
+    o3d.visualization.draw_geometries([downpcd])
 
+    # radius outlier removal
+    st = time.time()
+    fil_cl, ind = downpcd.remove_radius_outlier(nb_points=16, radius=0.2)
+    outlier_time = time.time() - st
+    print('outlier removal time: ' + str(outlier_time))
+    print()
+    o3d.visualization.draw_geometries([fil_cl])
+        
+    # DBSCAN clustering
+    st = time.time()
+    with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
+        labels = np.array(fil_cl.cluster_dbscan(eps=0.2, min_points=10, print_progress=False))
+    cluster_time = time.time() - st
+    print("cluster computing time: " + str(cluster_time))
+    print()
 
-# bounding volumes
-print('Bounding prism')
-st = time.time()
-aabb = cl.get_axis_aligned_bounding_box()
-bounding_time = time.time() - st
-print('axis aligned bounding box runtime: ' + str(bounding_time))
-aabb.color = (1, 0, 0)
-'''
-st = time.time()
-obb = pcd.get_oriented_bounding_box()
-print('oriented bounding box runtime: ' + str(time.time()-st))
-obb.color = (0, 1, 0)
-'''
-o3d.visualization.draw_geometries([cl, aabb],
-                                  zoom=0.7,
-                                  front=[0.5439, -0.2333, -0.8060],
-                                  lookat=[2.4615, 2.1331, 1.338],
-                                  up=[-0.1781, -0.9708, 0.1608])
+    max_label = labels.max()
+    colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+    colors[labels < 0] = 0
+    fil_cl.colors = o3d.utility.Vector3dVector(colors[:, :3])
+    o3d.visualization.draw_geometries([fil_cl])
 
-print('total time: ' + str(downsample_time + outlier_time + bounding_time + cluster_time))
+    # cluster segmentation
+    st = time.time()
+    N = labels.max() + 1
+    centroids, clusters = segment_clusters(N, fil_cl.points, labels)
+    segment_time = time.time() - st 
+    print("segmentation time: " + str(segment_time))
+    print()
+
+    # bounding volumes
+    st = time.time()
+    bd_boxes = [pcd.get_axis_aligned_bounding_box() for pcd in clusters]
+    bd_extents = [box.get_extent() for box in bd_boxes]
+    bounding_time = time.time() - st
+    print('axis aligned bounding box runtime: ' + str(bounding_time))
+    print()
+
+    for box in bd_boxes:
+        box.color = (1, 0, 0)
+    o3d.visualization.draw_geometries(bd_boxes + [fil_cl])
+                                    
+    print('total time: ' + str(downsample_time + outlier_time + bounding_time + cluster_time + segment_time))
+    print()
