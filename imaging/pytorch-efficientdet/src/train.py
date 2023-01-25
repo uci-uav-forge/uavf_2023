397d97271029206545f98b8a094d6fc0ac58a94d
Eric Pedley <ericpedley@gmail.com>
2023-01-24 19:57:58 -0800

    add tensorboard logger

--- imaging/pytorch-efficientdet/src/train.py
+++ imaging/pytorch-efficientdet/src/train.py
@@ -2,14 +2,13 @@
 from multiprocessing import freeze_support
 from pathlib import Path
 
-import numpy as np
 import torch
 import torch.onnx
 from data_utils import CarsDatasetAdaptor, EfficientDetDataModule
 import pandas as pd
 from model import EfficientDetModel
 from pytorch_lightning import Trainer
-
+from pytorch_lightning.loggers import TensorBoardLogger
 
 if __name__ == '__main__':
     freeze_support()# crashes without this for multi gpu train
@@ -24,20 +23,20 @@ if __name__ == '__main__':
             num_workers=4,
             batch_size=4)
 
-
+    backbone_name = "efficientnet_b0"
     model = EfficientDetModel(
         num_classes=13,
         img_size=512,
-        model_architecture="efficientnet_b0" # this is the name of the backbone. For some reason it doesn't work with the corresponding efficientdet name.
-        )
-    trainer = Trainer(
-            # strategy="dp",
-            strategy="ddp_find_unused_parameters_false",
-            gpus=[0,1], max_epochs=50, num_sanity_val_steps=1
+        model_architecture=backbone_name # this is the name of the backbone. For some reason it doesn't work with the corresponding efficientdet name.
         )
 
+    logger = TensorBoardLogger("tb_logs", name="UAV Forge Shape Detection")
+    num_epochs = 5
+    trainer = Trainer(
+            logger=logger,
+            strategy="ddp_find_unused_parameters_false",
+            gpus=[0,1], max_epochs=num_epochs, num_sanity_val_steps=1
+        )
+    # to upload logs: tensorboard dev upload --logdir tb_logs
     trainer.fit(model, dm)
-    # dummy_input = np.zeros(shape=model)
-    # torch.onnx.export(model)
-    torch.save(model.state_dict(), 'efficientdet_b0_pytorch_50epoch')
-    print("done")
+    torch.save(model.state_dict(), f'{backbone_name}_pytorch_{num_epochs}epoch.pt')
