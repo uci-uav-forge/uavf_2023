8e1a0c250164f8f32d2c90ab61571101b5d8d245
Eric Pedley <ericpedley@gmail.com>
2023-01-25 23:43:52 -0800

    rename dataset adaptor

--- imaging/pytorch-efficientdet/src/test.py
+++ imaging/pytorch-efficientdet/src/test.py
@@ -4,7 +4,7 @@ import math
 import torchmetrics.detection.mean_ap as mean_ap
 import torch
 import torch.onnx
-from data_utils import CarsDatasetAdaptor
+from data_utils import ShapeDatasetAdaptor
 import pandas as pd
 from model import EfficientDetModel
 from pprint import pprint
@@ -16,12 +16,13 @@ if __name__ == '__main__':
 
     test_annotations = pd.read_csv(dataset_path/'testannotations.csv')
     test_data_path = dataset_path/'test'
-    test_ds = CarsDatasetAdaptor(test_data_path, test_annotations)
+    test_ds = ShapeDatasetAdaptor(test_data_path, test_annotations)
 
     num_samples = len(test_ds.images)
-    batch_size = 30
+    batch_size = 4
+    num_batches = math.ceil(num_samples/batch_size)
 
-    for batch_idx in range(math.ceil(num_samples/batch_size)):
+    for batch_idx in range(num_batches):
         img_labels_list = [test_ds.get_image_and_labels_by_idx(i) for i in range(batch_idx*batch_size,min(num_samples,(batch_idx+1)*batch_size))]
 
         model = EfficientDetModel(
@@ -29,8 +30,10 @@ if __name__ == '__main__':
             img_size=512,
             model_architecture="efficientnet_b0" # this is the name of the backbone. For some reason it doesn't work with the corresponding efficientdet name.
             )
-        model.load_state_dict(torch.load('efficientdet_b0_pytorch_50epoch'))
+        model_file='efficientnet_b0_pytorch_50epoch_2.pt'
+        model.load_state_dict(torch.load(model_file))
         model.eval()
+        model.to(device="cuda")
 
         images = [x[0] for x in img_labels_list]
         actual_labels = [x[2].values for x in img_labels_list]
@@ -53,4 +56,5 @@ if __name__ == '__main__':
                 for labels, boxes in zip(actual_labels, actual_boxes)
             ],
         )
+        print(f"Finished {batch_idx}/{num_batches} batches")
     pprint(metric.compute())
--- imaging/pytorch-efficientdet/src/train.py
+++ imaging/pytorch-efficientdet/src/train.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import torch
 import torch.onnx
-from data_utils import CarsDatasetAdaptor, EfficientDetDataModule
+from data_utils import ShapeDatasetAdaptor, EfficientDetDataModule
 import pandas as pd
 from model import EfficientDetModel
 from pytorch_lightning import Trainer
@@ -14,12 +14,15 @@ if __name__ == '__main__':
     freeze_support()# crashes without this for multi gpu train
     dataset_path = Path('../data-gen/output')
 
-    df = pd.read_csv(dataset_path/'trainannotations.csv')
+    train_annotations = pd.read_csv(dataset_path/'trainannotations.csv')
     train_data_path = dataset_path/'train'
-    cars_train_ds = CarsDatasetAdaptor(train_data_path, df)
+    val_annotations = pd.read_csv(dataset_path/'validationannotations.csv')
+    val_data_path = dataset_path/'validation'
+    train_ds = ShapeDatasetAdaptor(train_data_path, train_annotations)
+    val_ds = ShapeDatasetAdaptor(val_data_path, val_annotations)
 
-    dm = EfficientDetDataModule(train_dataset_adaptor=cars_train_ds, 
-            validation_dataset_adaptor=cars_train_ds,
+    dm = EfficientDetDataModule(train_dataset_adaptor=train_ds, 
+            validation_dataset_adaptor=val_ds,
             num_workers=4,
             batch_size=4)
 
@@ -29,9 +32,10 @@ if __name__ == '__main__':
         img_size=512,
         model_architecture=backbone_name # this is the name of the backbone. For some reason it doesn't work with the corresponding efficientdet name.
         )
-
+    print(model.hparams_initial)
+    quit()
     logger = TensorBoardLogger("tb_logs", name="UAV Forge Shape Detection")
-    num_epochs = 5
+    num_epochs = 50
     trainer = Trainer(
             logger=logger,
             strategy="ddp_find_unused_parameters_false",
@@ -39,4 +43,4 @@ if __name__ == '__main__':
         )
     # to upload logs: tensorboard dev upload --logdir tb_logs
     trainer.fit(model, dm)
-    torch.save(model.state_dict(), f'{backbone_name}_pytorch_{num_epochs}epoch.pt')
+    # torch.save(model.state_dict(), f'{backbone_name}_pytorch_{num_epochs}epoch_2.pt')
