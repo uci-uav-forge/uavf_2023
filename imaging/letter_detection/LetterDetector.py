7144fcd454ce969d6f9a32954d498205d9d0f7e1
Eric Pedley <ericpedley@gmail.com>
2023-01-29 22:32:21 -0800

    add shape and letter to pipeline

--- imaging/letter_detection/LetterDetector.py
+++ imaging/letter_detection/LetterDetector.py
@@ -1,31 +1,31 @@
 import tensorflow as tf
 import numpy as np
-from PIL import Image
-image = Image.open('./test/dataset/data/0.jpg')
+
 class LetterDetector:
     def __init__(self, path):
         self.model = tf.keras.models.load_model(path)
-        self.model.summary()
         self.labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
 
     def predict(self, img):
-        image = tf.keras.utils.load_img(img, color_mode = "grayscale")
-        input_arr = tf.keras.utils.img_to_array(image)
-        input_arr = np.array([input_arr])
-        result = self.model.predict(input_arr)
-        print(result)
-        max = 0
-        max_col = 0
-        for col in range(len(result[0])):
-            if result[0][col] > max:
-                max = result[0][col]
-                max_col = col
-        print(f"Max: {self.labels[max_col]}: {max}")
+        result = self.model.predict(img)
+        # max = 0
+        # max_col = 0
+        # for col in range(len(result[0])):
+        #     if result[0][col] > max:
+        #         max = result[0][col]
+        #         max_col = col
+        return result
+        # print(f"Max: {self.labels[max_col]}: {max}")
 
 
 if __name__ == "__main__":
-    model = LetterDetector('trained_model.h5')
-    model.predict('./test/dataset/data/0.jpg')
+    model = LetterDetector('../trained_model.h5')
+    print(model.model.summary())
+    image = tf.keras.utils.load_img('./test/dataset/data/0.jpg', color_mode = "grayscale")
+    input_arr = tf.keras.utils.img_to_array(image)
+    input_arr = np.array([input_arr])
+    print(input_arr.shape)
+    # model.predict(input_arr)
     # ds_test = tf.data.Dataset.from_tensor_slices((test_file_paths, test_labels))
     # ds_test = ds_test.map(test_read_image).batch(2)
     # model.predict(ds_test)
--- imaging/main.py
+++ imaging/main.py
@@ -6,12 +6,16 @@
 
 import sys
 sys.path.append("..")
-from navigation.guided_mission.run_mission import Localizer
-import letter_detection.LetterDetector
+# from ..navigation.guided_mission.run_mission import Localizer
+import letter_detection.LetterDetector as letter_detection
+import shape_detection.src.model as shape_model
+import torch
 import time
 import cv2 as cv
 import os
-
+import numpy as np
+from PIL import Image
+import tensorflow as tf
 
 class Pipeline:
 
@@ -35,8 +39,28 @@ class Pipeline:
     """
 
     def __init__(self):
-        self.localizer = Localizer()
+        gpus = tf.config.list_physical_devices('GPU')
+        if gpus: # https://www.tensorflow.org/guide/gpu#limiting_gpu_memory_growth
+            tf.config.set_logical_device_configuration(
+                gpus[0],
+                [tf.config.LogicalDeviceConfiguration(memory_limit=1024)])
+
+        # self.localizer = Localizer()
+        self.tile_resolution=512# has to match img_size of the model, which is determined by which one we use.
+        backbone_name="efficientnet_b0"
+        model = shape_model.EfficientDetModel(
+            num_classes=13,
+            img_size=512,
+            model_architecture=backbone_name # this is the name of the backbone. For some reason it doesn't work with the corresponding efficientdet name.
+            )
+        model_file=f"shape_detection/src/efficientnet_b0_pytorch_25epoch.pt"
+        model.load_state_dict(torch.load(model_file))
+        model.to("cuda")
+        model.eval()
+        self.shape_model = model
+
         self.letter_detector = letter_detection.LetterDetector("trained_model.h5")
+
 
     
     def getCurrentLocation(self):
@@ -60,40 +84,80 @@ class Pipeline:
         f.write("Save counter: {} | location: {}\n".format(counter, loc))
         f.close()
 
+    def loop(self):
+        # ret, img = self.cam.read()
+        # if not ret: raise Exception("Failed to grab frame")
+        # current_location = self.getCurrentLocation()
+        # self.logGeolocation(save_counter, img, current_location)
+        img = cv.imread("gopro-image-5k.png")
+        h,w = img.shape[:2]
+        n_horizontal_tiles = w//self.tile_resolution
+        n_vertical_tiles = h//self.tile_resolution
+        all_tiles = []
+        h_tiles = np.split(img,range(self.tile_resolution,(n_horizontal_tiles+1)*self.tile_resolution,self.tile_resolution),axis=1)
+        tile_offsets_x_y: 'list[tuple]'  = []
+
+        for i,h_tile in enumerate(h_tiles):
+            y_offset = i*self.tile_resolution
+            v_tiles = np.split(h_tile,range(self.tile_resolution,(n_vertical_tiles+1)*self.tile_resolution,self.tile_resolution),axis=0)
+            for j,tile in enumerate(v_tiles):
+                all_tiles.append(tile)
+                tile_offsets_x_y.append((j*self.tile_resolution,y_offset))
+
+        pil_images = [Image.fromarray(tile) for tile in all_tiles]
+        batch_size = 1
+
+        bboxes, labels, confidences = [], [], [] # bboxes are xyxy
+        # `map(list,...` makes sure the correct `predict` type overload (List[PIL.Image]) is being called since it tries using an identity function for ndarray[PIL.Image]
+        for batch in map(list,np.split(
+            ary=pil_images, 
+            indices_or_sections=range(batch_size, len(pil_images),batch_size),
+            axis=0)
+        ):
+            res = self.shape_model.predict(batch)
+            b,l,c=res
+            bboxes.extend(b)
+            labels.extend(l)
+            confidences.extend(l)
+        print(tile_offsets_x_y[:5])
+        for tile_index in range(len(bboxes)):
+            if len(bboxes[tile_index])>0:
+                print(tile_index, bboxes[tile_index])
+                label = labels[tile_index]
+                x_offset, y_offset = tile_offsets_x_y[tile_index]
+                cropped_image = cv.cvtColor(
+                    src=np.array(pil_images[tile_index]),
+                    code=cv.COLOR_RGB2GRAY
+                )
+                just_letter_images=[]
+                for box_x0, box_y0, box_x1, box_y1 in bboxes[tile_index]:
+                    box_w=int(box_x1)-int(box_x0)
+                    box_h=int(box_y1)-int(box_y0)
+                    box_crop=cropped_image[
+                        int(box_x0):int(box_x1),
+                        int(box_y0):int(box_y1)
+                        ]
+                    print(box_w, box_h, box_crop.shape)
+                    just_letter_images.append(
+                        np.pad(box_crop,pad_width=((0,128-box_w),(0,128-box_h)))
+                    )
+                just_letter_images=np.array(just_letter_images)
+                result = self.letter_detector.predict(just_letter_images) # if this is slow try batching across multiple images. Maybe keep a queue of bbox crop sections and predict on the batch when a threshold is reached?
+                print([self.letter_detector.labels[np.argmax(row)] for row in result])
+
+        #     print(tile_index, result)
+
+
+        # time.sleep(self.SLEEP_TIME)
+
 
     def run(self):
         """
         Main run loop for the Imaging pipeline.
         """
-        save_counter = 0
         while True:
-            ret, img = self.cam.read()
-            if not ret: raise Exception("Failed to grab frame")
-            current_location = self.getCurrentLocation()
-            self.logGeolocation(save_counter, img, current_location)
-
-            """
-            self.shapeInference.makePrediction(img)
-            bounding_boxes = self.shapeInference.getBoundingBoxes() # Multiple bboxes per shape currently
-
-            # Create target list
-            targets = [Target(bounding_box=bbox) for bbox in bounding_boxes]
-
-            for target in targets:
-                bbox = target.bounding_box
-                y_start, y_end = bbox[0], bbox[2]
-                x_start, x_end = bbox[1], bbox[3]
-                cropped_image = img[y_start:y_end, x_start:x_end]
-                cv.imshow("Annotated image", cropped_image.astype(np.uint8))
-
-            """
-            """
-            prediction = self.letter_detector.predict(cropped_image)
-            print(prediction)
-            
-            """
-            time.sleep(self.SLEEP_TIME) # loop delay
+            self.loop()
 
 if __name__ == "__main__":
     imagingPipeline = Pipeline()
-    imagingPipeline.run()
+    imagingPipeline.loop()
