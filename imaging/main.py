c8368d53e32caec2057b32617f0760088f5608ae
Eric Pedley <ericpedley@gmail.com>
2023-01-30 23:37:41 -0800

    add cv plot function and complete tiling

--- imaging/main.py
+++ imaging/main.py
@@ -9,13 +9,15 @@ sys.path.append("..")
 # from ..navigation.guided_mission.run_mission import Localizer
 import letter_detection.LetterDetector as letter_detection
 import shape_detection.src.model as shape_model
+import shape_detection.src.plot_functions as plot_fns
 import torch
-import time
 import cv2 as cv
 import os
 import numpy as np
 from PIL import Image
+import json
 import tensorflow as tf
+import itertools
 
 class Pipeline:
 
@@ -47,13 +49,13 @@ class Pipeline:
 
         # self.localizer = Localizer()
         self.tile_resolution=512# has to match img_size of the model, which is determined by which one we use.
-        backbone_name="efficientnet_b0"
+        backbone_name="efficientnet_lite0"
         model = shape_model.EfficientDetModel(
             num_classes=13,
             img_size=512,
             model_architecture=backbone_name # this is the name of the backbone. For some reason it doesn't work with the corresponding efficientdet name.
             )
-        model_file=f"shape_detection/src/efficientnet_b0_pytorch_25epoch.pt"
+        model_file=f"shape_detection/src/efficientnet_lite0_pytorch_25epoch.pt"
         model.load_state_dict(torch.load(model_file))
         model.to("cuda")
         model.eval()
@@ -61,6 +63,14 @@ class Pipeline:
 
         self.letter_detector = letter_detection.LetterDetector("trained_model.h5")
 
+        with open("./shape_detection/data-gen/shape_name_labels.json","r") as f:
+            raw_dict: dict = json.load(f)
+            int_casted_keys = map(int, raw_dict.keys())
+            self.labels_to_names_dict = dict(zip(int_casted_keys, raw_dict.values()))
+
+        self.targets = [
+            ("Square","Red","O","White")
+        ]
 
     
     def getCurrentLocation(self):
@@ -84,14 +94,7 @@ class Pipeline:
         f.write("Save counter: {} | location: {}\n".format(counter, loc))
         f.close()
 
-    def loop(self):
-        # if you need to profile use this: https://stackoverflow.com/a/62382967/14587004
-
-        # ret, img = self.cam.read()
-        # if not ret: raise Exception("Failed to grab frame")
-        # current_location = self.getCurrentLocation()
-        # self.logGeolocation(save_counter, img, current_location)
-        img = cv.imread("gopro-image-5k.png")
+    def _split_to_tiles(self, img: cv.Mat):
         h,w = img.shape[:2]
         n_horizontal_tiles = w//self.tile_resolution
         n_vertical_tiles = h//self.tile_resolution
@@ -103,14 +106,51 @@ class Pipeline:
             y_offset = i*self.tile_resolution
             v_tiles = np.split(h_tile,range(self.tile_resolution,(n_vertical_tiles+1)*self.tile_resolution,self.tile_resolution),axis=0)
             for j,tile in enumerate(v_tiles):
+                if any(dim==0 for dim in tile.shape):
+                    continue
                 all_tiles.append(tile)
                 tile_offsets_x_y.append((j*self.tile_resolution,y_offset))
+        return (all_tiles, tile_offsets_x_y)
+
+    def _get_letter_crops(self, image: Image, bboxes: 'list[list[float]]'):
+        grayscale_image = cv.cvtColor(
+            src=np.array(image),
+            code=cv.COLOR_RGB2GRAY
+        )
+        just_letter_images=[]
+        for box_x0, box_y0, box_x1, box_y1 in bboxes:
+            box_w=int(box_x1)-int(box_x0)
+            box_h=int(box_y1)-int(box_y0)
+            if box_w>self.tile_resolution or box_h>self.tile_resolution:
+                continue
+            box_crop=grayscale_image[
+                int(box_y0):int(box_y1),
+                int(box_x0):int(box_x1)
+                ]
+            just_letter_images.append(
+                np.pad(box_crop,pad_width=((0,128-box_h),(0,128-box_w)))
+            )
+        return np.array(just_letter_images)
+
+    def loop(self):
+        # if you need to profile use this: https://stackoverflow.com/a/62382967/14587004
+
+        # ret, img = self.cam.read()
+        # if not ret: raise Exception("Failed to grab frame")
+        # current_location = self.getCurrentLocation()
+        # self.logGeolocation(save_counter, img, current_location)
+        img = cv.imread("gopro-image-5k.png")
+
+        all_tiles, tile_offsets_x_y =self._split_to_tiles(img)
 
         pil_images = [Image.fromarray(tile) for tile in all_tiles]
         batch_size = 1
 
         bboxes, labels, confidences = [], [], [] # bboxes are xyxy
         # `map(list,...` makes sure the correct `predict` type overload (List[PIL.Image]) is being called since it tries using an identity function for ndarray[PIL.Image]
+
+        offset_corrected_bboxes = []
+        split_indices =  list()
         for batch in map(list,np.split(
             ary=pil_images, 
             indices_or_sections=range(batch_size, len(pil_images),batch_size),
@@ -120,32 +160,24 @@ class Pipeline:
             b,l,c=res
             bboxes.extend(b)
             labels.extend(l)
-            confidences.extend(l)
-        print(tile_offsets_x_y[:5])
+            confidences.extend(c)
         for tile_index in range(len(bboxes)):
-            if len(bboxes[tile_index])>0:
-                print(tile_index, bboxes[tile_index])
-                label = labels[tile_index]
-                x_offset, y_offset = tile_offsets_x_y[tile_index]
-                cropped_image = cv.cvtColor(
-                    src=np.array(pil_images[tile_index]),
-                    code=cv.COLOR_RGB2GRAY
-                )
-                just_letter_images=[]
-                for box_x0, box_y0, box_x1, box_y1 in bboxes[tile_index]:
-                    box_w=int(box_x1)-int(box_x0)
-                    box_h=int(box_y1)-int(box_y0)
-                    box_crop=cropped_image[
-                        int(box_x0):int(box_x1),
-                        int(box_y0):int(box_y1)
-                        ]
-                    print(box_w, box_h, box_crop.shape)
-                    just_letter_images.append(
-                        np.pad(box_crop,pad_width=((0,128-box_w),(0,128-box_h)))
-                    )
-                just_letter_images=np.array(just_letter_images)
-                result = self.letter_detector.predict(just_letter_images) # if this is slow try batching across multiple images. Maybe keep a queue of bbox crop sections and predict on the batch when a threshold is reached?
-                print([self.letter_detector.labels[np.argmax(row)] for row in result])
+            if len(bboxes[tile_index])<=0:
+                continue
+            shape_predictions = [self.labels_to_names_dict[int(x)] for x in labels[tile_index]]
+
+            y_offset,x_offset = tile_offsets_x_y[tile_index]
+
+            just_letter_images = self._get_letter_crops(pil_images[tile_index], bboxes[tile_index])
+
+            result = self.letter_detector.predict(just_letter_images) # if this is slow try batching across multiple images. Maybe keep a queue of bbox crop sections and predict on the batch when a threshold is reached?
+            letter_predictions = [self.letter_detector.labels[np.argmax(row)] for row in result]
+
+            print(list(zip(shape_predictions,letter_predictions)))
+
+            for box_x0, box_y0, box_x1, box_y1 in bboxes[tile_index]:
+                offset_corrected_bboxes.append([box_x0+x_offset,box_y0+y_offset, box_x1+x_offset, box_y1+y_offset])
+        plot_fns.show_image_cv(img, offset_corrected_bboxes,list(itertools.chain(*labels)),list(itertools.chain(*confidences)),labels_dict=self.labels_to_names_dict,file_name="processed_img.png",font_scale=1,thickness=2,box_color=(0,0,255),text_color=(0,0,0))
 
         #     print(tile_index, result)
 
@@ -160,6 +192,9 @@ class Pipeline:
         while True:
             self.loop()
 
-if __name__ == "__main__":
+def main():
     imagingPipeline = Pipeline()
     imagingPipeline.loop()
+
+if __name__ == "__main__":
+    main()
