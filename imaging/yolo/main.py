fdd66045479732495f7cd28345e713ff50347ed7
Eric Pedley <ericpedley@gmail.com>
2023-02-09 22:42:30 -0800

    added yolo training script and script to format data correctly

---
+++ imaging/yolo/main.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO # pip install ultralytics (tested with python 3.8)
+# https://github.com/ultralytics/yolov5/wiki/Train-Custom-Data
+# Load a model
+# model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov5n.pt")  # load a pretrained model (recommended for training)
+
+# Train the model
+model.train(data ="forge10k.yaml", epochs=100, imgsz=512)
+model.save("yolov5n_forge10k.pt")
