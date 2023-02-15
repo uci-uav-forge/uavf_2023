0cbf54ede59ef97f25fb521459e22165370afc84
Shrijani <vemulapally.shrijani@gmail.com>
2023-02-14 18:30:10 -0800

    Added numpy arrays with vectorized code

--- navigation/obs_avoid/obstacle_avoidance.py
+++ navigation/obs_avoid/obstacle_avoidance.py
@@ -1,78 +1,46 @@
 import math
-class obstacle:
-  def __init__(self, pos, rad):
-    self.position = pos
-    self.radius = rad
-    self.velocity = [0, 0, 0]
-  
-  def get_position(self):
-    return self.position
-  
-  def set_position(self, pos):
-    self.position = pos
-  
-  def get_radius(self):
-    return self.radius
-  
-  def set_radius(self, rad):
-    self.radius = rad
+import numpy as np
 
-  def get_velocity(self):
-    return self.velocity
-  
-  def set_velocity(self, vel):
-    self.velocity = vel
+def obstacle_avoidance(centeroids, dimensions):
+  #R = 10
+  angle = 45
+  path_distance = 1
+  drone_distance = 1
 
-def distance(position):
-  x,y = position
-  dist = math.sqrt(x**2 + y**2)
-  return dist
+  cur_heading = 0
+  obstacle_avoidance = False
 
+  radius_list = np.amax(dimensions, axis=1) #list of all the radius's
+  distance_list = np.linalg.norm(centeroids, axis=1) #distance of all the objects
+  path_dist = np.abs(centeroids[:,0]) #list of all the abs(x) values
+  if(np.any(path_dist< radius_list+path_distance) or np.any(distance_list < radius_list + drone_distance)):
+    obstacle_avoidance = True
 
-
-def avoidObstacle(objects): #objects should be a list of obstacles
-  R = 10 #meters, radius of field view
-  angle = 45 #degrees, field view angle
-  path_distance = 3 #meters, safety distance from the path
-  drone_distance = 3 #meters, safety distance from drone
-
-  cur_heading = 0 #degrees, drone's current heading
-  avoidObstacle = False
-
-  for obj in objects:
-    drone_dist = distance(obj.get_position()) #use radial distance function 
-    path_dist = abs(obj.get_position()[0]) 
-    if (path_dist < obj.get_radius() + path_distance) or (distance(obj.get_position()) < drone_distance + obj.get_radius()):  
-      avoidObstacle = True
-      break
-
-  
-  while avoidObstacle and cur_heading < angle:
+  while obstacle_avoidance and cur_heading < angle:
     cur_heading += 10
-    avoidObstacle = False
+    obstacle_avoidance = False
 
-    for obj in objects:
-      drone_dist = distance(obj.get_position()) #use  radial distance function 
-      beta = math.atan(obj.get_position()[0]/obj.get_position()[1]) - math.radians(cur_heading)
-      path_dist = drone_dist * math.sin(beta) #take absolute value here
-      if (path_dist < obj.get_radius() + path_distance) or (drone_dist < drone_distance + obj.get_radius()): #safety distance from path 
-        avoidObstacle = True
-        break
+
+    radius_list = np.amax(dimensions, axis=1) #list of all the radius's
+    distance_list = np.linalg.norm(centeroids, axis=1) #distance of all the objects
+    beta_list = np.arctan2(centeroids[:,0], centeroids[:,1]) - np.radians(cur_heading)
+    path_dist = np.abs(distance_list * np.sin(beta_list))
+    if(np.any(path_dist< radius_list+path_distance) or np.any(distance_list < radius_list + drone_distance)):
+      obstacle_avoidance = True
+    
   if(cur_heading > 45):
     return 45
   else:
     return cur_heading
 
-def main(centers, dimensions):
-  objects = []
-  for c, d in zip(centers, dimensions):
-    obs = obstacle(c, d)
-    objects.append(obs)
 
-  drone_heading = avoidObstacle(objects)
+
+def main(centers, dimensions):
+  drone_heading = obstacle_avoidance(centers, dimensions)
   print(drone_heading)
 
+
 if __name__ == " __main__":
-    centers = [[0,5]]
-    dimensions = [0.25]
-    main(centers, dimensions)
+  centers = np.array([[0,6], [0,5]])
+  dimensions = np.array([[0.25, 0.1, 0.1], [1.0, 0.25, 0.25]])
+  main(centers, dimensions)
