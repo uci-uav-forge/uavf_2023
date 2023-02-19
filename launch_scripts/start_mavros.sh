2d1959d7ddb3fe622034eea772ca08327b9ef517
Derek Fan <herpderkfanie@gmail.com>
2023-02-19 08:54:00 -0800

    confirmed shell scripts working with python3 command and tmux

--- launch_scripts/start_mavros.sh
+++ launch_scripts/start_mavros.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 # shell script for launching mavros
 roslaunch mavros px4.launch fcu_url:="/dev/ttyPixhawk"
-
--- launch_scripts/start_mission.sh
+++ launch_scripts/start_mission.sh
@@ -4,5 +4,6 @@
 #NOTE: cd is set such that this script is only possible to run in the ~/uavf_2023/ directory, ideally by being called by launch_ver2.sh 
 #attempting to call this script from any other directory will fail
 
-cd navigation/guided_mission
-OPENBLAS_CORETYPE=ARMV8 python guided_mission.py
+cd navigation
+#OPENBLAS_CORETYPE=ARMV8 python guided_mission.py
+python3 -m guided_mission.guided_mission
