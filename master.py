eae17e946b6d109479dad58ea93085d62d798c46
Nano <tommydneill@gmail.com>
2023-02-21 14:52:11 -0800

    add ros node to master script

--- master.py
+++ master.py
@@ -5,8 +5,9 @@ Currently only running Imaging pipeline to test for Feb. 16 flight day.
 
 from imaging.yolo_main import Pipeline
 from navigation.guided_mission.run_mission import Localizer
-
+import rospy
 if __name__ == "__main__":
+    rospy.init_node("imaging_pipeline", anonymous=True)
     localizer = Localizer()
     imaging_pipeline = Pipeline(localizer)
     imaging_pipeline.run()
