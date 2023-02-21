1f4aafc7f1083a279800255582f9d92e8c2d19e3
Eric Pedley <ericpedley@gmail.com>
2023-02-21 15:26:11 -0800

    add data gen and train script for yolo segmentation

--- imaging/yolo/train.py
+++ imaging/yolo/train.py
@@ -4,8 +4,8 @@ task='segmentation'
 if task=='segmentation':
     model = YOLO('yolov8n-seg.yaml')
 
-    model.train(data='coco128-seg.yaml', epochs=100)
+    model.train(data='forge10k-seg.yaml', epochs=100)
 elif task=='detection':
     model = YOLO('yolov8n.yaml')
 
-    model.train(data='forge10k.yaml', epochs=100)
+    model.train(data='forge10k.yml', epochs=100)
