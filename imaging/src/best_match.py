16adb68a3c3af2d9ff849a457e3069105a00acd8
adilquamar <32375723+adilquamar@users.noreply.github.com>
2023-02-24 11:25:12 -0800

    added python file to host the best match algorithm

---
+++ imaging/src/best_match.py
@@ -0,0 +1,5 @@
+"""
+Determines the best match target for a given payload drop solely based on shape and letter predictions.
+Without UNET, color predictions are unreliable. Decision to select target for payload drop must be made without
+shape and letter predictions.
+"""
