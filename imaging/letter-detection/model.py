9786ee3c2fa8ffab43a38836ed386cfa51bc746d
Shannon Liou <sch9050@gmail.com>
2023-03-16 17:58:24 -0700

    Added letter_model_gpu.onnx

--- imaging/letter-detection/model.py
+++ imaging/letter-detection/model.py
@@ -116,9 +116,9 @@ def Convert_ONNX():
     # Export the model   
     torch.onnx.export(model,         # model being run 
          dummy_input,       # model input (or a tuple for multiple inputs) 
-         "LetterModel.onnx",       # where to save the model  
+         "letter_model_gpu.onnx",       # where to save the model  
          export_params=True,  # store the trained parameter weights inside the model file 
-         opset_version=13,    # the ONNX version to export the model to 
+         #opset_version=13,    # the ONNX version to export the model to 
          do_constant_folding=True,  # whether to execute constant folding for optimization 
          input_names = ['modelInput'],   # the model's input names 
          output_names = ['modelOutput'], # the model's output names 
