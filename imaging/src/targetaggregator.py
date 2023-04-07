4999bfc3957a9382e563dfc6b3e5837e29197b36
adilquamar <32375723+adilquamar@users.noreply.github.com>
2023-04-07 15:11:35 -0700

    targetaggreg code cleanup

--- imaging/src/targetaggregator.py
+++ imaging/src/targetaggregator.py
@@ -5,17 +5,18 @@ import webcolors
 
 
 def color_dist(rgbA, rgbB):
-    #todo - possibly measure color distance in some more sophisticated way.
-    return sum((a - b)**2 for (a,b) in zip(rgbA, rgbB))
-    
+    # TODO: measure color distance in some more sophisticated way.
+    return sum((a - b) ** 2 for (a, b) in zip(rgbA, rgbB))
+
 
 def gen_color_conf(rgb, cnames):
     r0 = {
-        color : color_dist(rgb, webcolors.name_to_rgb(color))
+        color: color_dist(rgb, webcolors.name_to_rgb(color))
         for color in cnames}
-    
+
     mx = max(r0.values())
-    return {k: v/mx for k,v in r0.items()} 
+    return {k: v / mx for k, v in r0.items()}
+
 
 class TargetAggregator:
     def __init__(self, targets_file):
@@ -23,19 +24,18 @@ class TargetAggregator:
             self.targets = list(map(tuple, csv.reader(tf)))
         self.n_targets = len(self.targets)
         self.best_conf = [-1] * self.n_targets
-        self.target_gps = [None] * self.n_targets
-    
+        self.target_gps = [None] * self.n_targets  # corresponds to list of targets in CSV
+
     def match_target_color(self, gps, letterColor, letterConf, shapeColor, shapeConf):
         letterColorConf = gen_color_conf(letterColor, [x[0] for x in self.targets])
         shapeColorConf = gen_color_conf(shapeColor, [x[2] for x in self.targets])
         match, score = best_match_color(self.targets, letterColorConf, letterConf, shapeColorConf, shapeConf)
         matchIndex = self.targets.index(match)
 
-
         if score > self.best_conf[matchIndex]:
             self.target_gps[matchIndex] = gps
             self.best_conf[matchIndex] = score
-    
+
     def match_target(self, gps, lC, sC):
         match, score = best_match(self.targets, lC, sC)
         matchIndex = self.targets.index(match)
@@ -43,7 +43,6 @@ class TargetAggregator:
         if score > self.best_conf[matchIndex]:
             self.target_gps[matchIndex] = gps
             self.best_conf[matchIndex] = score
-
 
 
 if __name__ == '__main__':
