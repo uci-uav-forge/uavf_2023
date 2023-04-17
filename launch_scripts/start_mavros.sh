5e2d120e95b60ee5888408659264c871e4bb8c02
Nano <tommydneill@gmail.com>
2023-04-17 10:46:24 -0700

    get rid of old scripts also update comment on mavros start script

--- launch_scripts/start_mavros.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# shell script for launching mavros
-roslaunch mavros px4.launch fcu_url:="/dev/ttyPixhawk"
--- launch_scripts/start_recorder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#shell script for running guided_mission python file
-#NOTE: cd is set such that this script is only possible to run in the ~/uavf_2023/ directory, ideally by being called by launch_ver2.sh 
-#attempting to call this script from any other directory will fail
-
-#OPENBLAS_CORETYPE=ARMV8 python guided_mission.py
-python3 -m navigation.data_collection.telem_recorder
