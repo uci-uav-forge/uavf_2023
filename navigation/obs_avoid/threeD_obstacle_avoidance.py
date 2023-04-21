668ec2103bbd08740f6da2494f3fe50853ae8b73
Derek Fan <herpderkfanie@gmail.com>
2023-04-20 21:09:14 -0700

    new mission json, revised obstacle avoidance

--- navigation/obs_avoid/threeD_obstacle_avoidance.py
+++ navigation/obs_avoid/threeD_obstacle_avoidance.py
@@ -3,82 +3,86 @@ import numpy as np
 import math
 import random
 from time import time
+from numba import njit, prange
+
+
+def path_is_safe(hdg, mag_arr, radius_arr, Xs, Ys, path_dist):
+  phis = np.abs(hdg - (90 - np.degrees(np.arctan(Ys/Xs))))
+  D_diff = mag_arr*(np.sin(np.radians(phis))) - radius_arr
+  masked_D_diff = np.ma.masked_greater(D_diff, path_dist)
+  bool_arr = np.ma.getmaskarray(masked_D_diff)
+  return np.all(bool_arr)
 
 
 def obstacle_avoidance(centroids, dimensions, max_hdg):
-  R = 16     # m
-  increment = 3  # deg
-
-  path_distance = 1 # m
-  drone_distance = 1 # m
+  increment = 5  # deg
+  path_dist = 1 # m
+  drone_dist = 1
   z_dist = 1 # m
 
-  #check if any z coordinates are above the boundry and also chek if half the z dimension is intersecting the boundry
+  print(centroids)
+  print()
+
+  #check if any z coordinates are above the boundry and also check if half the z dimension is intersecting the boundry
   c_new = []
   d_new = []
 
-  for coords, dims in zip(centroids[:], dimensions[:]):
-    if(np.abs(coords[2]) < z_dist or np.abs(coords[2])- dims[2]/2 < z_dist):
-      c_new.append(coords)
-      d_new.append(dims)
-  centroids = np.array(c_new)
-  dimensions = np.array(d_new)
-  #remove that object from the obstacle list
+  #for coords, dims in zip(centroids[:], dimensions[:]):
+  for i in range(len(centroids)):
+    if (centroids[i][2] >= 0 and centroids[i][2]-dimensions[i][2] < z_dist)\
+    or (centroids[i][2] < 0 and centroids[i][2]+dimensions[i][2] > -z_dist):
+      c_new.append(centroids[i])
+      d_new.append(dimensions[i])
 
+  centr_arr = np.array(c_new)
+  dim_arr = np.array(d_new)
+  print(centr_arr)
+
+  # approximate max len dimension as sphereical radius
+  # if no dangers present, skip obstacle avoidance
   try:
-    radius_list = np.amax(dimensions, axis=1) #list of all the radius's, max of all the dimensions
-    distance_list = np.linalg.norm(centroids, axis=1) #distance of all the objects
+    radius_arr = np.amax(dim_arr, axis=1) 
   except np.AxisError:
     return False
   
+  twoD_centroids = np.delete(centr_arr, 2, axis=1)
+  mag_arr = np.zeros(len(twoD_centroids))
+  for i in range(len(twoD_centroids)):
+    mag_arr[i] = np.sqrt(twoD_centroids[i].dot(twoD_centroids[i]))
+  
+  x_centrs = twoD_centroids[:,0]
+  y_centrs = twoD_centroids[:,1]
   right_hdg = 0
   left_hdg = 0
-  
+
   # sweep right quadrant
-  while right_hdg < max_hdg:
-    beta_list = np.arctan2(centroids[:,0], centroids[:,1]) - np.radians(right_hdg)
-    path_dist = np.abs(distance_list * np.sin(beta_list))
-    if not (np.any(path_dist< radius_list+path_distance) or np.any(distance_list < radius_list + drone_distance)):
+  while right_hdg < max_hdg + 1:
+    if path_is_safe(right_hdg, mag_arr, radius_arr, x_centrs, y_centrs, path_dist):
       break
     right_hdg += increment
   
   # sweep left quadrant
-  while left_hdg > -max_hdg:
-    beta_list = np.arctan2(centroids[:,0], centroids[:,1]) - np.radians(left_hdg)
-    path_dist = np.abs(distance_list * np.sin(beta_list)) 
-    if not (np.any(path_dist< radius_list+path_distance) or np.any(distance_list < radius_list + drone_distance)):
+  while left_hdg > -max_hdg - 1:
+    if path_is_safe(left_hdg, mag_arr, radius_arr, x_centrs, y_centrs, path_dist):
       break
     left_hdg -= increment
-  
-  # take the smallest heading deviation
+
+  print(left_hdg)
+  print(right_hdg)
   if abs(left_hdg) < right_hdg: return left_hdg
   else: return right_hdg
 
 
-def main(centers, dimensions):
-  drone_heading = obstacle_avoidance(centers, dimensions)
-  print(drone_heading)
-
-
-def function3():
-  centers = []
-  dimensions = []
-  for i in range(60):
-      a = random.randint(-30, 30)
-      b = random.randint(0, 15)
-      f = random.randint(-30, 30)
-      centers.append([a, b, f])
-      c = random.random()
-      d = random.random()
-      e = random.random()
-      dimensions.append([c,d,e])
-  #print(centers)
-  #print(dimensions)
-  st = time()
-  main(np.array(centers), np.array(dimensions))
-  et= time()
-  print(et-st)
-
-
 if __name__=='__main__':
-  function3()
+  #centr_arr = np.array([[2, 16, 0]] )
+  #dim_arr = np.array([[8, 8, 8]])
+  n=50
+  centr_arr = 6*np.abs(np.random.randn(n,3))
+  dim_arr = np.full((n,3), 2)
+
+  st = time()
+
+  hdg = obstacle_avoidance(centr_arr, dim_arr, 43)
+  print(hdg)
+  
+  print(time()-st)
