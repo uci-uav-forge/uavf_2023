1c84a568689d330aabea54055cce02d8c785405a
Eric Pedley <ericpedley@gmail.com>
2023-04-22 01:47:25 -0700

    make image size param less ambiguous

--- imaging/local_geolocation.py
+++ imaging/local_geolocation.py
@@ -8,6 +8,8 @@ class GeoLocator:
 
     def get_location(self, image_x: int, image_y: int, location:"tuple[float,float,float]", angles:"tuple[float,float,float]", img_size: "tuple[int,int]") -> "tuple[float,float,float]":
         """
+        img_size should be (height, width)
+
         Returns the location of a pixel in the image in the world frame.
         location: assumed to be (x,y,z) where z is the height
         angles: assumed to be (heading, tilt, roll)
@@ -27,7 +29,8 @@ class GeoLocator:
             ct.RectilinearProjection(                
                 view_x_deg=73,
                 view_y_deg=58,
-                image=img_size),
+                image_width_px=img_size[1],
+                image_height_px=img_size[0]),
             ct.SpatialOrientation(
                 elevation_m=z,
                 heading_deg=heading,
