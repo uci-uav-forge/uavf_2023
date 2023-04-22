725525a659d38ea06d94c55cf7df026912804ee5
Eric Pedley <ericpedley@gmail.com>
2023-04-21 23:07:11 -0700

    add extra train arguments

--- imaging/yolo/train.py
+++ imaging/yolo/train.py
@@ -6,4 +6,13 @@ if task=='segmentation':
 elif task=='detection':
     model = YOLO('yolov8n.yaml')
     
-model.train(data='forge10k.yaml', epochs=100)
+model.train(
+    data='forge10k.yaml', 
+    epochs=100, 
+    save=True,
+    save_period=10,
+    workers=4,
+    cos_lr=True,
+    overlap_mask=False,
+    # device=[0,1]
+)
