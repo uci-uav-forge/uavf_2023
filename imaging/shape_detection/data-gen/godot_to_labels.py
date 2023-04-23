b9453f7fa585cb848dab14b290d52eab22e8764d
Eric Pedley <ericpedley@gmail.com>
2023-04-22 17:34:12 -0700

    make data preprocessing parallelized 🚀

--- imaging/shape_detection/data-gen/godot_to_labels.py
+++ imaging/shape_detection/data-gen/godot_to_labels.py
@@ -5,6 +5,8 @@ import cv2
 import json
 import numpy as np
 from tqdm import tqdm
+from multiprocessing.pool import Pool
+from functools import partial
 
 def preprocess_img(img):
     # blur image with random kernel size
@@ -20,8 +22,36 @@ def preprocess_img(img):
     img = np.clip(img, 0, 255)
     return img
 
+def gen_img(num, num_images, input_dir, output_dir, shapes_to_categories):
+    if int(num)<0.85*num_images:
+        split_name = "train"
+    elif int(num)<0.95*num_images:
+        split_name = "validation"
+    else:
+        split_name = "test" 
+    img = cv2.imread(f"{input_dir}/images/image{num}.png")
+    if img is None:
+        tqdm.write(f"image read error for {img}")
+        return
+    img = preprocess_img(img)
+    file_contents = ""
+    for mask_file_name in os.listdir(f"{input_dir}/masks/{num}"):
+        mask_path = f"{input_dir}/masks/{num}/{mask_file_name}"
+        shape_name = mask_file_name.split("_")[0] 
+        mask = cv2.imread(mask_path)
+        polygon = get_polygon(mask)
 
-if __name__ == "__main__":
+        if len(polygon) <= 2:
+            if os.getenv("VERBOSE") is not None:
+                tqdm.write(f"no polygon found for {mask_path}")
+            return 
+        normalized_polygon = polygon / np.array([mask.shape[1], mask.shape[0]])
+        file_contents+=f"{shapes_to_categories[shape_name]} {' '.join(map(str, normalized_polygon.flatten()))}\n"
+    with open(f"{output_dir}/labels/{split_name}/image{num}.txt", "w") as f:
+        f.write(file_contents)
+    cv2.imwrite(f"{output_dir}/images/{split_name}/image{num}.png", img)
+
+def main():
     datagen_dir = os.path.dirname(os.path.abspath(__file__))
     categories_to_shapes = json.load(open(f"{datagen_dir}/shape_name_labels.json","r"))
     shapes_to_categories = {shape:category for category, shape in categories_to_shapes.items()}
@@ -32,31 +62,12 @@ if __name__ == "__main__":
         os.makedirs(f"{output_dir}/labels/{split_name}", exist_ok=True)
         os.makedirs(f"{output_dir}/images/{split_name}", exist_ok=True)
     num_images = len(os.listdir(f"{input_dir}/images"))
-    for num in tqdm(range(num_images)):
-        if int(num)<0.85*num_images:
-            split_name = "train"
-        elif int(num)<0.95*num_images:
-            split_name = "validation"
-        else:
-            split_name = "test" 
-        img = cv2.imread(f"{input_dir}/images/image{num}.png")
-        if img is None:
-            tqdm.write(f"image read error for {img}")
-            continue
-        img = preprocess_img(img)
-        cv2.imwrite(f"{output_dir}/images/{split_name}/image{num}.png", img)
-        f = open(f"{output_dir}/labels/{split_name}/image{num}.txt", "w")
-        for mask_file_name in os.listdir(f"{input_dir}/masks/{num}"):
-            mask_path = f"{input_dir}/masks/{num}/{mask_file_name}"
-            shape_name = mask_file_name.split("_")[0] 
-            mask = cv2.imread(mask_path)
-            polygon = get_polygon(mask)
 
-            if len(polygon) <= 2:
-                tqdm.write(f"no polygon found for {mask_path}")
-                continue
-            normalized_polygon = polygon / np.array([mask.shape[1], mask.shape[0]])
-            f.write(f"{shapes_to_categories[shape_name]} {' '.join(map(str, normalized_polygon.flatten()))}\n")
-        f.close()
+    gen_images_w_context = partial(gen_img, num_images=num_images, input_dir=input_dir, output_dir=output_dir, shapes_to_categories=shapes_to_categories)
+    with Pool(32) as p:
+        results = tqdm(p.imap(gen_images_w_context, range(num_images)), total=num_images)
+        tuple(results)
+if __name__ == "__main__":
+    main()
 
     
