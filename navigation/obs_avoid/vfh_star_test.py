6b711fef503b377c319f98abcacde9b7873e3e99
thomas neill <tommydneill@gmail.com>
2023-08-28 19:10:22 -0700

    cleanup test code

--- navigation/obs_avoid/vfh_star_test.py
+++ navigation/obs_avoid/vfh_star_test.py
@@ -1,11 +1,12 @@
 from .vfh_star import *
+from .vfh_star_2d import *
 
 import open3d as o3d
 import matplotlib.pyplot as plt
+import pprofile
+import functools
 
-
-if __name__ == '__main__':
-    params = VFHParams( \
+default_params = VFHParams( \
         D_t = 0.5,
 
         N_g = 3,
@@ -34,6 +35,41 @@ if __name__ == '__main__':
         dir_spacing = 2
     )
 
+def run_test(VFHImpl, histogram, drone_pos, phi, theta, t_pos, suffix):
+    print("="*10)
+    print(f"test{suffix}")
+    vfh = VFHImpl(default_params, histogram)
+    pos = np.array(drone_pos)
+
+    reslt = vfh.gen_polar_histogram(pos)
+    np.savetxt(f'polar_hist{suffix}', reslt, delimiter=',', newline='\n')
+
+    
+
+    reslt2 = vfh.gen_bin_histogram(reslt)
+    np.savetxt(f'bin_hist{suffix}', reslt2, delimiter=',', newline='\n')
+    reslt3 = vfh.gen_masked_histogram(reslt2,pos,theta,phi)
+    np.savetxt(f'masked_hist{suffix}', reslt3, delimiter=',', newline='\n')
+
+    print(vfh.gen_directions(reslt3, t_pos - pos))
+    with pprofile.Profile() as prof:
+        reslt = vfh.get_target_dir(pos, theta, phi,  t_pos)
+        prof.dump_stats(f'profdump{suffix}')
+    print(vfh.theta_phi_to_dxyz(*reslt))
+
+
+class VFH2DWrapper(VFH2D):
+    def get_masked_histogram(self, a,b,c, *args):
+        super().get_masked_histogram(self, a,b,c)
+    def get_target_dir(self, position: np.ndarray, theta: float, *args) -> tuple[float, float]:
+        return [super().get_target_dir(position, theta, args[-1])]
+    def gen_masked_histogram(self, bin_hist: np.ndarray, pos: np.array, theta: float, *args) -> np.ndarray:
+        return super().gen_masked_histogram(bin_hist, pos, theta)
+    def theta_phi_to_dxyz(self, theta):
+        return super().theta_to_dxyz(theta)
+
+
+if __name__ == '__main__':
     drone_pos = [-5,0,0]
 
     radius = 0.5
@@ -46,18 +82,20 @@ if __name__ == '__main__':
             self.scene = o3d.t.geometry.RaycastingScene()
             for mesh in self.meshes:
                 self.scene.add_triangles(mesh)
-        def get_confidence(self, indices):
-            indices2 = indices * params.hist_res
+        @functools.lru_cache(maxsize=None)
+        def confinner(self, indices):
+            indices = np.array(indices)
+            indices2 = indices * default_params.hist_res
             query_point = o3d.core.Tensor([list(indices2)], dtype=o3d.core.Dtype.Float32)
             if self.scene.compute_signed_distance(query_point) < 0:
                 return 0.8
             return 0    
 
+        def get_confidence(self, indices):
+            return self.confinner(tuple(indices))
 
     meshes = [sphere_mesh]
     dh = MeshTestHistogram(meshes)
-
-    vfh = VFH(params, dh)
 
     display_scene = o3d.t.geometry.RaycastingScene()
     for mesh in meshes:
@@ -65,9 +103,9 @@ if __name__ == '__main__':
     display_scene.add_triangles(o3d.t.geometry.TriangleMesh.create_sphere(radius=radius).translate(drone_pos))
 
     rays = o3d.t.geometry.RaycastingScene.create_rays_pinhole(
-        fov_deg=90,
+        fov_deg = 90,
         center=[0, 0, 0],
-        eye= drone_pos + [-1,1,0],
+        eye= np.array(drone_pos) + np.array([-1,1,0]),
         up=[0, 1, 0],
         width_px=640,
         height_px=480,
@@ -76,27 +114,12 @@ if __name__ == '__main__':
     ans = display_scene.cast_rays(rays)
     plt.imshow(ans['t_hit'].numpy())
     plt.show()
-    
-
-    pos = np.array(drone_pos)
 
     phi = 0
     theta = 0 # + x direction
     t_pos = np.array([40,0,0])
-
-    reslt = vfh.gen_polar_histogram(pos)
-    np.savetxt('hist.dump', reslt, delimiter=',', newline='\n')
-
     
-
-    reslt2 = vfh.gen_bin_histogram(reslt)
-    np.savetxt('hist.dump2', reslt2, delimiter=',', newline='\n')
-    reslt3 = vfh.gen_masked_histogram(reslt2,pos,theta,phi)
-    np.savetxt('hist.dump3', reslt3, delimiter=',', newline='\n')
-
-    print(vfh.gen_directions(reslt3, t_pos - pos))
-    reslt = vfh.get_target_dir(pos, theta, phi,  t_pos)
-    print(vfh.theta_phi_to_dxyz(*reslt))
+    reslt = run_test(VFH2DWrapper, MeshTestHistogram(meshes), drone_pos, phi, theta, t_pos, '_open3d')
 
     
     class ParallelWalls:
@@ -105,7 +128,7 @@ if __name__ == '__main__':
             self.width = width
             self.height = height
 
-        def get_confidence(self,indices, width, height):
+        def get_confidence(self,indices):
             if (indices[0] == -self.width // 2 or indices[0] == self.width // 2) and -self.height // 2 < indices[2] < self.height // 2:
                 return 0.7
             return 0
@@ -113,24 +136,10 @@ if __name__ == '__main__':
     
     pwh = ParallelWalls(np.array([0, 0, 0]), 20, 20)
 
-    vfh = VFH(params, pwh)
-
     pos = np.array([-10,0,0])
 
     phi = 0
     theta = 0 # + x direction
     t_pos = np.array([40,0,0])
 
-    reslt = vfh.gen_polar_histogram(pos)
-    np.savetxt('hist_pw.dump', reslt, delimiter=',', newline='\n')
-
-
-    reslt2 = vfh.gen_bin_histogram(reslt)
-    np.savetxt('hist_pw.dump2', reslt2, delimiter=',', newline='\n')
-    reslt3 = vfh.gen_masked_histogram(reslt2,pos,theta,phi)
-    np.savetxt('hist_pw.dump3', reslt3, delimiter=',', newline='\n')
-
-    print(vfh.gen_directions(reslt3, t_pos - pos))
-    reslt = vfh.get_target_dir(pos, theta, phi,  t_pos)
-    print(vfh.theta_phi_to_dxyz(*reslt))
-
+    reslt = run_test(VFH, pwh, pos, phi, theta, t_pos, "_par_walls")
